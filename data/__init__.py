68395d3f64079fe62f1930193b6252e7df8dd6da
Eitam Shafran <eitam.sh@gmail.com>
2023-08-14 10:53:12 +0200

    Add get data loaders for CV

--- data/__init__.py
+++ data/__init__.py
@@ -3,9 +3,9 @@ from .MyDataset import CustomDataset
 from torch.utils.data import DataLoader, random_split
 from logger import logging
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from torchvision import transforms
-from .utils import preprocess
+from .utils import preprocess, preprocess_cv
 
 
 def get_dataloaders(df_dir,params):
@@ -37,3 +37,21 @@ def get_dataloaders(df_dir,params):
     val_dl = DataLoader(val_ds, batch_size=params["batch_size"], shuffle=False)
    
     return train_dl, val_dl
+
+def get_dataloaders_cv(df_dir,params):
+    
+    """
+    This function returns the train and val dataloaders.
+    """
+
+    # create the datasets
+    df = pd.read_csv(df_dir)
+
+
+    ## preprocess data
+    train_folds, val_folds = preprocess_cv(df, params, normalization = params["normalization"])
+
+
+    # logging.info(f'Train samples={len(train_folds[1])}, Validation samples={len(val_folds[1])}')
+   
+    return train_folds, val_folds
