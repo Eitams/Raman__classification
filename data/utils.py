1906ad630644127c4c738aa45ab65917a58621a2
Eitam Shafran <eitam.sh@gmail.com>
2023-08-14 10:53:52 +0200

    Add preprocess for CV

--- data/utils.py
+++ data/utils.py
@@ -1,8 +1,10 @@
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import scipy.signal as signal
+from .MyDataset import CustomDataset
+from torch.utils.data import DataLoader
 
 
 def preprocess_raman_spectrum(df):
@@ -109,4 +111,70 @@ def preprocess(df, params, normalization=True):
 
 
     return train_df, val_df
+
+def preprocess_cv(df, params, normalization=True):
+    start = params["start_wave"]
+    end = params["end_wave"]
+    test_size = params["test_size"]
+    seed = params["seed"]
+    
+    ## preprocess data
+    df.columns = df.iloc[0] # Set First row as column names
+    df.drop(0, axis=0, inplace = True) # Drop first row
+   # df.drop(df.columns[0], axis=1, inplace = True) # Drop patient id column
+    df.columns = ["patient", "diabetic"] + list(df.columns[2:]) # Change names of first column
+
+    ## Slice df to wanted wave boundaries
+    tmp =df["diabetic"]
+    df = df.iloc[:, start+1:end+2]
+    df.insert(loc=0, column='diabetic', value=tmp)
+    df = df.reset_index().drop("index", axis = 1)
+    # Perform K-Fold Cross-Validation
+    num_folds = 7
+    train_folds = {}
+    val_folds = {}
+    
+    kf = KFold(n_splits=num_folds, shuffle=True, random_state=seed)
+    for fold, (train_index, test_index) in enumerate(kf.split(df)):
+        
+        if normalization:
+            # Create data loaders for the current fold
+            train_df, val_df = df.iloc[train_index,1:-1], df.iloc[test_index,1:-1]
+            ## Normalize data
+        
+            # assigning df without the target column to preprocess function
+            preprocessed_train_df = preprocess_raman_spectrum(train_df.iloc[:,1:])
+            preprocessed_val_df = preprocess_raman_spectrum(val_df.iloc[:,1:])
+            # print(train_index)
+            # print(df.head())
+            # print(df.loc[train_index,"diabetic"])
+            # # print(preprocessed_train_df)
+            # print(pd.concat([df.loc[train_index,"diabetic"].reset_index(), preprocessed_train_df], axis=1).set_index("index"))
+
+
+            # Mergin processed data frame with target column
+            train_df = pd.concat([df.loc[train_index,"diabetic"].reset_index(), preprocessed_train_df], axis=1).set_index("index")
+            val_df = pd.concat([df.loc[test_index,"diabetic"].reset_index(), preprocessed_val_df], axis=1).set_index("index")
+ 
+            #scalar = MinMaxScaler()
+            #train_scaled = pd.DataFrame(scalar.fit_transform(train_df.iloc[:,1:]), columns = train_df.columns[1:])
+            #val_scaled = pd.DataFrame(scalar.transform(val_df.iloc[:,1:]), columns = val_df.columns[1:])
+            
+            #train_df = pd.concat([train_df["diabetic"].reset_index(), train_scaled], axis=1).set_index("index")
+            #val_df = pd.concat([val_df["diabetic"].reset_index(), val_scaled], axis=1).set_index("index")
+        
+        else:
+            # Create data loaders for the current fold
+            train_df, val_df = df.iloc[train_index,:-1], df.iloc[test_index,:-1]
+
+        train_ds = CustomDataset(train_df)
+        val_ds = CustomDataset(val_df)
+
+        train_dl = DataLoader(train_ds, batch_size=params["batch_size"], shuffle=True)
+        val_dl = DataLoader(val_ds, batch_size=params["batch_size"], shuffle=False)
+        
+        train_folds[fold+1] = train_dl
+        val_folds[fold+1] = val_dl
+
+    return train_folds, val_folds
 
